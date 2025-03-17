30c19430ba633ab90dcc4f45cd99fea01de7a8ad
lloladin123 <blackiealexnader@yahoo.dk>
2025-03-17 12:43:08 +0100

    cleanup

--- app/Store/Slices/instructorSlice.ts
+++ app/Store/Slices/instructorSlice.ts
@@ -1,24 +1,71 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { Instructor } from "../../Types/index";
+import { PlanningEvent, Instructor } from "../../Types/index";
 
-const initialState: Instructor[] = [
-  { id: 1, name: "Alexander" },
-  { id: 2, name: "Lisa" },
-  { id: 3, name: "Noah" },
-  { id: 4, name: "Eliza" },
-  { id: 5, name: "Agnes" },
-  { id: 6, name: "Liv" },
-];
+const initialState: PlanningEvent[] = [];
 
-const instructorSlice = createSlice({
-  name: "instructors",
+const eventSlice = createSlice({
+  name: "events",
   initialState,
   reducers: {
-    addInstructor: (state, action: PayloadAction<Instructor>) => {
+    setEvents: (state, action: PayloadAction<PlanningEvent[]>) => {
+      return action.payload;
+    },
+    addEvent: (state, action: PayloadAction<PlanningEvent>) => {
       state.push(action.payload);
+    },
+    initializeEvents: (state, action: PayloadAction<Instructor[]>) => {
+      const instructors = action.payload;
+
+      return [
+        {
+          id: 1,
+          title: "Springmusene",
+          instructors: instructors.slice(0, 6),
+          type: "practice",
+          description: "bob",
+          date: new Date(),
+          location: "Kildegården 9",
+        },
+        {
+          id: 2,
+          title: "Supernovaer",
+          instructors: instructors.slice(0, 5),
+          type: "practice",
+          description: "bob",
+          date: new Date(),
+          location: "Kildegården 9",
+        },
+        {
+          id: 3,
+          title: "Stjerneskud",
+          instructors: instructors.slice(0, 4),
+          type: "practice",
+          description: "bob",
+          date: new Date(),
+          location: "Kildegården 9",
+        },
+        {
+          id: 4,
+          title: "Springmix",
+          instructors: instructors.slice(0, 3),
+          type: "practice",
+          description: "bob",
+          date: new Date(),
+          location: "Kildegården 9",
+        },
+        {
+          id: 5,
+          title: "Power Boys",
+          instructors: instructors.slice(0, 2),
+          type: "practice",
+          description: "bob",
+          date: new Date(),
+          location: "Kildegården 9",
+        },
+      ];
     },
   },
 });
 
-export const { addInstructor } = instructorSlice.actions;
-export default instructorSlice.reducer;
+export const { setEvents, addEvent, initializeEvents } = eventSlice.actions;
+export default eventSlice.reducer;
