35a4b8fff0390c428ffe1492f24e8392686a5662
lloladin123 <blackiealexnader@yahoo.dk>
2025-03-31 12:52:58 +0200

    import error

--- app/Components/Nav.tsx
+++ app/Components/Nav.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import React, { useState } from "react";
-import { NavItem } from "./types";
+import { NavItem } from "../types";
 
 const InitialNavLinks: NavItem[] = [
   {
