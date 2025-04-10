ea9f09164a4a7c6d4787edbc66a0d36a45c97712
lloladin123 <blackiealexnader@yahoo.dk>
2025-04-10 11:21:31 +0200

    made proper file structure for mockdata by splitting each data type into a seperate file

---
+++ app/data/MockData/mockInstructorData.ts
@@ -0,0 +1,10 @@
+import { Instructor } from "../../Types/index";
+
+export const mockInstructorsData: Instructor[] = [
+  { id: 1, name: "Alexander", email: "email@bob.dk", address: "address1", phone: 22949292, type: "Instructor" },
+  { id: 2, name: "Lisa", email: "email@bob.dk", address: "address1", phone: 22949292, type: "Instructor" },
+  { id: 3, name: "Noah", email: "email@bob.dk", address: "address1", phone: 22949292, type: "Instructor" },
+  { id: 4, name: "Eliza", email: "email@bob.dk", address: "address1", phone: 22949292, type: "Instructor" },
+  { id: 5, name: "Agnes", email: "email@bob.dk", address: "address1", phone: 22949292, type: "Instructor" },
+  { id: 6, name: "Liv", email: "email@bob.dk", address: "address1", phone: 22949292, type: "Instructor" },
+];
