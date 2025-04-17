77aa835cd73f40627cd48943abe0b94064449e9a
lloladin123 <blackiealexnader@yahoo.dk>
2025-04-17 11:44:48 +0200

    added a login/logout button that dynamically change based on auth.currentUser state

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -1,20 +1,44 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Nav from "../Components/Nav";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const auth = getAuth();
 
 const Header = () => {
+  const [user, setUser] = useState(auth.currentUser);
+
+  // Detect auth state
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      console.log("Auth changed:", user ? "Logget ind" : "Ikke logget ind");
+    });
+
+    return () => unsubscribe(); // clean up listener
+  }, []);
+
   return (
     <header className="px-10">
       <div className="flex justify-between items-center py-5">
         <h1 className="text-4xl">Gymnastic Instructor planning tool</h1>
-        <Link
-          className="bg-gray-800 text-white p-2 rounded-md"
-          href="Pages/CreateEvent"
-        >
-          Add New Event
-        </Link>
+        <div className="flex flex-row space-x-2 justify-between items-center">
+          {auth.currentUser ? (
+            <Link href="/login">Logout</Link>
+          ) : (
+            <Link href="/login">Login</Link>
+          )}
+          <Link
+            className="bg-gray-800 text-white p-2 rounded-md"
+            href="Pages/CreateEvent"
+          >
+            Add New Event
+          </Link>
+        </div>
       </div>
+
       <div className="flex space-x-2 items-center">
         <Image
           width={40}
